bceb7557fd612b9a550228107adaed0f67f29bc0
cat-elena <162091381+elena7130@users.noreply.github.com>
2024-03-31 11:02:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Cute Coloringpages', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Elena Qu🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.cute-coloringpages', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'www.cute-coloringpages.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'cutecoloringpages,coloringpages,butterflycoloringpages', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
